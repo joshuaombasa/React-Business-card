ac64f6da12c87627d80be506798f2c514c225d6a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-22 14:52:40 +0300

    added about component

---
+++ components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function About() {
+    return (
+        <div className="about-container">
+          <div className="inner-about-container">
+               <h4 className="about-header">About</h4>
+               <p className="about-paragraph">I am a frontend developer with a partuclar interest in making things simple and automating daily tasks. I keep and with security and industry best prcatices, and I am lookings for new skills and tools to learn.</p>
+          </div>
+        </div>
+    )
+}
+
+export default About
