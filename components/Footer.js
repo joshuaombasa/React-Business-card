a33138c5d817128b8620e6ce6849f1390430cf40
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-22 14:53:25 +0300

    added Footer component

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ function Footer() {
+     return (
+         <div className="footer-container about-container">
+            <div className="footer-container-inner inner-about-container">
+             <div className="icon-cotainer"><i class="fa-brands fa-twitter"></i></div>
+             <div className="icon-cotainer"><i class="fa-brands fa-facebook"></i></div>
+             <div className="icon-cotainer"><i class="fa-brands fa-instagram"></i></div>
+             <div className="icon-cotainer"><i class="fa-brands fa-github"></i></div>
+         </div>
+         </div>
+     )
+ }
+ 
+ export default Footer
