447d004d59e665de17b6febd81e4f1975c0616d3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-22 14:53:03 +0300

    added App component

---
+++ components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Info from './Info.js'
+import About from './About.js'
+import  Interests from './Interests'
+import Footer from './Footer'
+
+function App() {
+    return (
+        <div>
+             <Info />
+             <About />
+             <Interests />
+             <Footer />
+        </div>
+    )
+}
+
+export default App 
