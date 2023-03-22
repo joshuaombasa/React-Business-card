af6fc64ba93f79cf185a7b365fdac8cd70fa17e7
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-22 14:53:50 +0300

    added Info component

---
+++ components/Info.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+ function Info() {
+     return (
+        <div className="info-container">
+          <img  src="/home/joshua/Downloads/pp (2).JPG"/>
+            <h3 className="userName">Joshua Ombasa</h3>
+            <p className="title">Frontend Developer</p>
+            <small className="emailadress">joshuaombasa.co.ke</small>
+            <div className="socials">
+                <p className="email"><i class="fa-solid fa-envelope"></i>Email</p>
+                <p className="linkedin"><i class="fa-brands fa-linkedin  icon-colors"></i>LinkedIn</p>
+            </div>
+        </div>
+     )
+ }
+ 
+ 
+ export default Info
