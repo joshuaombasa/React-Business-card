a726b14fff9f8674205a7d2b3a4900ad9c8fddaf
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-22 14:54:13 +0300

    added Interests component

---
+++ components/Interests.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Interests() {
+    return (
+       <div className="about-container">
+           <div className="inner-about-container">
+           <h4 className="about-header">Interests</h4>
+           <p className="about-paragraph">Learning new technology, attending tech meetups and conferances, solving hard problems for businesses, mentoring junior developers.</p>
+        </div>
+       </div>
+    )
+}
+
+export default Interests
